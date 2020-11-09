6a2c32dd9f7626c81f7f82922030aa15653d48ca
Ella Thompson <fang98t@gmail.com>
2020-11-09 11:51:59 -0500

    new questions

--- index.js
+++ index.js
@@ -21,60 +21,50 @@ var fs = require('fs');
 const questions = [
     {
         type: "input",
-        message: "What is your GitHub user name?",
+        message: "What is your name??",
         name: "username"
     },
 
     {
-        type: "input",
-        message: "What is your project Title?",
-        name: "title",
-        default: "Generate a README.md file "
+        type: 'input',
+        message: "What is your email?",
+        name: 'email'
     },
 
     {
-        type: "input",
-        message: "What is your repo called?",
-        name: "repo",
-        default: "GoodREADMEGenerator"
+        type: 'input',
+        message: "What is your Github username?",
+        name: 'username'
     },
 
     {
-        type: "input",
-        message: "How do you describe your Project?.",
-        name: "desc",
-        default:
-        " This application will generate a README.md file for your current project"
+        type: 'input',
+        message: "What is your ReadMe title?",
+        name: 'title'
     },
 
     {
-        type: "input",
-        message: "What are the steps required to install your project?",
-        name: "install",
-        default: "Step1: Run npm install and Step2: Run node index.js"
+        type: 'input', 
+        message: "Enter a brief description",
+        name: 'description'
     },
 
     {
-        type: "input",
-        message: "Write instructions for using your project.",
-        name: "usage",
-        default:
-        "1.Run node index.js 2.Answers the questions 3.The README.md file will be created. "
+        type: 'input',
+        message: "Are there any guidlines for how developers can contribute to your project?",
+        name: 'guidelines'
     },
 
     {
-        type: "input",
-        message:
-        "Please enter GitHub user names of the contributor if any. (If there are mulitple contributor, seperate names with comma and no space!)",
-        name: "contributors",
-        default: "Bharat Modi, Jaren Hubal and Claire Goray"
+        type: 'input',
+        message: "What is your name?",
+        name: 'name'
     },
 
     {
-        type: "input",
-        message: "Provide examples on how to run tests.",
-        name: "test",
-        default: "Insert your tests sample here..."
+        type: 'input',
+        message: "Provide any tests for your application on how to run them.",
+        name: 'tests',
     }
 ];
 
